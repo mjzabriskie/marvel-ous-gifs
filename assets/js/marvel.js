b112d4a070ed93f08862706bf96bbba1a8d00e1c
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-04-27 20:01:33 -0600

    created call to GIPHY and output results in console

--- assets/js/marvel.js
+++ assets/js/marvel.js
@@ -4,6 +4,8 @@ var searchInput = document.getElementById("search-input");
 
 const APIKey = "1c68710c9a12fca3d6066e8f1e1bc1c1";
 const hashKey = "acdbcd7e533a37b7ba8af93b84c3021e";
+const giphyKey = "l0bmAzCfm8fxpcpAusIYozKfaOUG4B22";
+
 var ts = 1;
 
 var getUserInput = function (event) {
@@ -12,6 +14,7 @@ var getUserInput = function (event) {
     if(heroName) {
         console.log(heroName);
         getMarvelData(heroName);
+        getGifs(heroName);
         searchInput.value = "";
     } else {
         alert("enter hero name");
@@ -27,6 +30,17 @@ var getMarvelData = function (hero) {
             });
         }
     });
+}
+
+var getGifs = function (hero) {
+    var apiUrl = "https://api.giphy.com/v1/gifs/search?api_key=l0bmAzCfm8fxpcpAusIYozKfaOUG4B22&q="+hero+"+cartoon&limit=6&offset=0&rating=pg-13&lang=en";
+    fetch(apiUrl).then(function(response) {
+        if(response.ok){
+            response.json().then(function(data){
+                console.log(data);
+            })
+        }
+    })
 }
 searchEl.addEventListener("submit", getUserInput);
 
