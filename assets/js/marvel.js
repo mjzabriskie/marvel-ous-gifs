66997a2c44e4a09c08168d932c4674df4a0e615a
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-04-30 11:53:55 -0600

    created function to display search results and take user to the next page

--- assets/js/marvel.js
+++ assets/js/marvel.js
@@ -1,5 +1,6 @@
 var searchEl = document.getElementById("user-form");
 var searchInput = document.getElementById("search-input");
+var resultsEl = document.querySelector("#results");
 
 
 const APIKey = "1c68710c9a12fca3d6066e8f1e1bc1c1";
@@ -7,6 +8,38 @@ const hashKey = "acdbcd7e533a37b7ba8af93b84c3021e";
 const giphyKey = "l0bmAzCfm8fxpcpAusIYozKfaOUG4B22";
 
 var ts = 1;
+
+var displayResults = function(hero){
+    //check if api returned any heros
+    if (hero.data.count === 0) {
+      resultsEl.textContent = "No hero found.";
+      return false;
+    }
+
+    //clear old content
+    resultsEl.textContent = "";
+
+    //loop over heros
+    for (var i = 0; i < hero.data.count; i++) {
+      //format repo name
+      var heroName = hero.data.results[i].name;
+
+      //create container for each repo
+      var heroResultEl = document.createElement("a");
+      heroResultEl.classList = "card-body";
+      heroResultEl.setAttribute("href", "./character-page.html?heroName=" + heroName);
+
+      //create span element to hold repo name
+      var heroNameEl = document.createElement("span");
+      heroNameEl.textContent = heroName;
+
+      //append to container
+      heroResultEl.appendChild(heroNameEl);
+
+      //append container to the dom
+      resultsEl.appendChild(heroResultEl);
+    }
+  };
 
 var getUserInput = function (event) {
     event.preventDefault();
@@ -28,12 +61,12 @@ $(function() {
           url: "https://gateway.marvel.com/v1/public/characters",
           dataType: "json",
           data: {
-            ts: 1,  
+            ts: 1,
             nameStartsWith: request.term,
             limit: 10,
             apikey: "1c68710c9a12fca3d6066e8f1e1bc1c1",
             hash: "acdbcd7e533a37b7ba8af93b84c3021e"
-            
+
           },
           success: function(data) {
             console.log(data);
@@ -54,11 +87,12 @@ $(function() {
   });
 
 var getMarvelData = function (hero) {
-    var apiUrl = "https://gateway.marvel.com/v1/public/characters?ts="+ ts +"&name=" + hero + "&apikey=" + APIKey + "&hash=" + hashKey;
+    var apiUrl = "https://gateway.marvel.com/v1/public/characters?ts="+ ts +"&nameStartsWith=" + hero + "&apikey=" + APIKey + "&hash=" + hashKey;
     fetch(apiUrl).then(function (response) {
         if(response.ok) {
             response.json().then(function (data) {
                 console.log(data);
+                displayResults(data);
             });
         }
     });
