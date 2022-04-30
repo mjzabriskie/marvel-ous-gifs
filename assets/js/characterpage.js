3d6b70195d1a2183d6dd512c5faabbffbc06cb07
Luis <luisriveros26@gmail.com>
2022-04-30 11:18:40 -0600

    second javascript page

---
+++ assets/js/characterpage.js
@@ -0,0 +1,43 @@
+const APIKey = "1c68710c9a12fca3d6066e8f1e1bc1c1";
+const hashKey = "acdbcd7e533a37b7ba8af93b84c3021e";
+var ts = 1;
+
+var getMarvelData = function () {
+    var hero = localStorage.getItem('search');
+    console.log(hero);
+    var apiUrl = "https://gateway.marvel.com/v1/public/characters?ts=" + ts + "&name=" + hero + "&apikey=" + APIKey + "&hash=" + hashKey;
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+                characterPage(data);
+                var apiUrl = "https://gateway.marvel.com/v1/public/characters/" + data.data.results[0].id + "/comics?" + "ts=" + ts + "&apikey=" + APIKey + "&hash=" + hashKey;
+                fetch(apiUrl).then(function (response) {
+                    if (response.ok) {
+                        response.json().then(function (dataId) {
+                            console.log(dataId);
+                            characterPage(data, dataId);
+                        });
+                    }
+                });
+            });
+        }
+
+    });
+};
+
+var characterPage = function (data, dataId) {
+    var characterName = data.data.results[0].name;
+    var characterBio = data.data.results[0].description;
+    var characterImg = data.data.results[0].thumbnail.path;
+    if(characterBio === "") {
+        console.log("empty");
+    }
+    console.log(characterImg);
+    $("#hero-img").attr("src", characterImg + ".jpg");
+    $("#hero-bio").text(characterBio);
+    $("#character-name").text(characterName);
+    console.log(characterName);
+};
+
+getMarvelData();
--- assets/js/marvel.js
+++ assets/js/marvel.js
@@ -1,7 +1,7 @@
 var searchEl = document.getElementById("user-form");
 var searchInput = document.getElementById("search-input");
 
-
+var search = [];
 const APIKey = "1c68710c9a12fca3d6066e8f1e1bc1c1";
 const hashKey = "acdbcd7e533a37b7ba8af93b84c3021e";
 var ts = 1;
@@ -9,8 +9,10 @@ var ts = 1;
 var getUserInput = function (event) {
     event.preventDefault();
     var heroName = searchInput.value.trim();
+    localStorage.setItem('search', heroName);
     if(heroName) {
         console.log(heroName);
+        
         getMarvelData(heroName);
         searchInput.value = "";
     } else {
@@ -51,7 +53,7 @@ $(function() {
   });
 
 var getMarvelData = function (hero) {
-    var apiUrl = "https://gateway.marvel.com/v1/public/characters?ts="+ ts +"&name=" + hero + "&apikey=" + APIKey + "&hash=" + hashKey;
+    var apiUrl = "https://gateway.marvel.com/v1/public/characters?ts="+ ts +"&nameStartsWith=" + hero + "&apikey=" + APIKey + "&hash=" + hashKey;
     fetch(apiUrl).then(function (response) {
         if(response.ok) {
             response.json().then(function (data) {
@@ -59,7 +61,11 @@ var getMarvelData = function (hero) {
             });
         }
     });
-}
+};
+
+
+
+
 searchEl.addEventListener("submit", getUserInput);
 
 
