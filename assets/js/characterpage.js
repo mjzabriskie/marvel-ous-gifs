2f63e936483800c2d928b61c1c38da1629a52d22
Luis <luisriveros26@gmail.com>
2022-05-01 14:17:51 -0600

    code added for when character doesn't have description

--- assets/js/characterpage.js
+++ assets/js/characterpage.js
@@ -1,3 +1,6 @@
+var bioContentEl = document.getElementById("bio-content");
+var comicEl = document.getElementById("results");
+
 const APIKey = "1c68710c9a12fca3d6066e8f1e1bc1c1";
 const hashKey = "acdbcd7e533a37b7ba8af93b84c3021e";
 var ts = 1;
@@ -13,8 +16,8 @@ var getMarvelData = function () {
         if (response.ok) {
             response.json().then(function (data) {
                 console.log(data);
-                characterPage(data);
-                var apiUrl = "https://gateway.marvel.com/v1/public/characters/" + data.data.results[0].id + "/comics?" + "ts=" + ts + "&apikey=" + APIKey + "&hash=" + hashKey;
+                // characterPage(data);
+                var apiUrl = "https://gateway.marvel.com/v1/public/characters/" + data.data.results[0].id + "/comics?" + "ts=" + ts + "&limit=5&apikey=" + APIKey + "&hash=" + hashKey;
                 fetch(apiUrl).then(function (response) {
                     if (response.ok) {
                         response.json().then(function (dataId) {
@@ -35,6 +38,40 @@ var characterPage = function (data, dataId) {
     var characterImg = data.data.results[0].thumbnail.path;
     if(characterBio === "") {
         console.log("empty");
+        bioContentEl.textContent = "";
+        for(var i = 0; i < dataId.data.count; i++) {
+            var comicTitle = dataId.data.results[i].title;
+            var comicImage = dataId.data.results[i].thumbnail.path;
+            var comicUrl = dataId.data.results[i].urls[0].url;
+            
+            var heroResultEl = document.createElement("a");
+            heroResultEl.classList = "cards";
+            heroResultEl.setAttribute("href", comicUrl);
+            heroResultEl.setAttribute("target", "_blank");
+            heroResultEl.setAttribute("id", "result-btn");
+
+            // create img element
+            var imgContainerEl = document.createElement("div")
+            var imgEl = document.createElement("img");
+            imgContainerEl.classList = "card-image";
+            imgEl.classList = "";
+            imgEl.setAttribute("src", comicImage + "/portrait_fantastic.jpg")
+            imgContainerEl.appendChild(imgEl);
+
+            //create span element to hold repo name
+            var heroNameEl = document.createElement("span");
+            heroNameEl.setAttribute("class", "hero-name-btn");
+            heroNameEl.classList = "hero-name-btn font-beba is-size-5"
+            heroNameEl.textContent = comicTitle;
+
+            //append to container
+            heroResultEl.appendChild(imgContainerEl);
+            heroResultEl.appendChild(heroNameEl);
+
+
+            //append container to the dom
+            comicEl.appendChild(heroResultEl);
+        }
     }
     $("#hero-img").attr("src", characterImg + "/portrait_uncanny.jpg");
     $("#hero-bio").text(characterBio);
